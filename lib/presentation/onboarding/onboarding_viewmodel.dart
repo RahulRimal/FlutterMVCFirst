9ff815812f4fd128a2aabb0e41111d8572cae72f
Rahul Rimal <genuinetechnology9@gmail.com>
2022-01-10 07:18:56 +0545

    Starting on network layer

---
+++ lib/presentation/onboarding/onboarding_viewmodel.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+
+import 'package:complete_advanced_flutter/domain/model.dart';
+import 'package:complete_advanced_flutter/presentation/base/baseviewmodel.dart';
+import 'package:complete_advanced_flutter/presentation/resources/assets_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
+
+class OnBoardingViewModel extends BaseViewModel
+    with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
+  final StreamController _streamController =
+      StreamController<SliderViewObject>();
+
+  late final List<SliderObject> _list;
+
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _streamController.close();
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+    _list = _getSliderData();
+
+    _postDateToView();
+  }
+
+  @override
+  int goNext() {
+    int nextIndex = _currentIndex++; // +1
+    if (nextIndex >= _list.length) {
+      _currentIndex = 0; // infinite loop to go to first item inside the slider
+    }
+
+    return _currentIndex;
+  }
+
+  @override
+  int goPrevious() {
+    int previousIndex = _currentIndex--; // -1
+    if (previousIndex == -1) {
+      _currentIndex =
+          _list.length - 1; // infinite loop to go to the length of slider list
+    }
+
+    return _currentIndex;
+  }
+
+  @override
+  void onPageChanged(int index) {
+    _currentIndex = index;
+    _postDateToView();
+  }
+
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => _streamController.sink;
+
+  @override
+  // TODO: implement outputSliderViewObject
+  Stream<SliderViewObject> get outputSliderViewObject =>
+      _streamController.stream.map((sliderViewObject) => sliderViewObject);
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingSubTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4)
+      ];
+
+  _postDateToView() {
+    inputSliderViewObject.add(
+        SliderViewObject(_list[_currentIndex], _list.length, _currentIndex));
+  }
+}
+
+abstract class OnBoardingViewModelInputs {
+  void goNext();
+  void goPrevious();
+  void onPageChanged(int index);
+
+  Sink get inputSliderViewObject;
+}
+
+abstract class OnBoardingViewModelOutputs {
+  Stream<SliderViewObject> get outputSliderViewObject;
+}
+
+class SliderViewObject {
+  SliderObject sliderObject;
+  int numberOfSlides;
+  int currentIndex;
+
+  SliderViewObject(this.sliderObject, this.numberOfSlides, this.currentIndex);
+}
